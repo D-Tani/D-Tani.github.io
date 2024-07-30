6510da9ed7cdc4e8cade0ba21950ca19316123ad
D-Tani <e2058132@soka-u.jp>
2024-07-31 03:37:59 +0900

    update

--- js/script.js
+++ js/script.js
@@ -4,39 +4,36 @@ document.addEventListener('DOMContentLoaded', () => {
             title: "1日目 上陸",
             date: "2023年8月29日",
             content: "単身で北海道に住む母に会うために初の北海道上陸。残暑が厳しい本州に対して、気温ほど暑さを感じさせない風が空港を出ると感じられた。",
-            image: "airport.jpg"
+            photos: ["airport.jpg"]
         },
         {
             title: "青い池",
             content: "水面がエメラルド色に見える観光名所。外国人が多く、様々な言語が飛び交っていました。",
-            image: "bluepond.jpg"
+            photos: ["bluepond.jpg"]
         },
         {
             title: "温泉の横にある森の家々",
             date: "",
             content: "",
-            image: "foresthouses.jpg"
+            photos: ["foresthouses.jpg"]
         },
         {
             title: "2日目 SHIRO みんなの工場",
             date: "2023年8月30日",
             content: "砂川にある国産フレグランスブランドの工場施設へ。やはり女性客が多くややアウェーな雰囲気を感じた。施設内のカフェでゆっくりとした時間を過ごし、お土産を買って後にした。",
-            image: "shirofactory.jpg",
-            image: "shirocafe.jpg"
+            photos: ["shirofactory.jpg","shirocafe.jpg"]
         },
         {
             title: "昼食",
             date: "",
             content: "何もない平原の中にポツンとある喫茶店で昼食。大地と空が近くさえ感じる景色を見ながら、柔らかい肉とピザトーストにかぶりついた。なんと贅沢な時間だろう。",
-            image: "coffee.jpg",
-            image2: "plate.jpg"
+            photos: ["coffee.jpg","plate.jpg"]
         },
         {
             title: "大自然",
             date: "",
             content: "帰りの飛行機の時間も迫ってきた頃、最後に北海道の大きさを体験することにした。あたり一面「何も無い」が広がる光景、東京では到底味わえない開放感が最高だった。",
-            image: "nature.jpg",
-            image2: "nature2.jpg"
+            photos: ["nature.jpg","nature2.jpg"]
         }
     ];
 
@@ -48,10 +45,6 @@ document.addEventListener('DOMContentLoaded', () => {
         const img = document.createElement('img');
         img.src = entry.image;
         img.alt = entry.title;
-
-        const img2 = document.createElement('img');
-        img2.src = entry.image;
-        img2.alt = entry.title;
 
         const h2 = document.createElement('h2');
         h2.textContent = entry.title;
